f63e656a9e7cfd4f5f52d125a52edbf6de3f7dd0
Isl0m <islomrustam09@gmail.com>
2024-04-03 14:27:12 +0500

    telegram webapps

--- src/components/webapps.tsx
+++ src/components/webapps.tsx
@@ -2,21 +2,13 @@
 
 import {
   useInitData,
-  useShowPopup,
   useThemeParams,
 } from "@vkruglikov/react-telegram-web-app";
 
 export function WebApps() {
   const [initDataUnsafe, initData] = useInitData();
   const [colorScheme, themeParams] = useThemeParams();
-  const showPopup = useShowPopup();
 
-  const color = getComputedStyle(document.documentElement).getPropertyValue(
-    "--tg-theme-bg-color"
-  );
-  if (color) {
-    showPopup({ message: color });
-  }
   if (themeParams.bg_color) {
     document.documentElement.style.setProperty(
       "--tg-theme-bg-color",
