2445235f9a9a250667a7450028975e11d75364b5
Isl0m <islomrustam09@gmail.com>
2024-04-04 00:18:52 +0500

    telegram webapps

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,20 +1,29 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { TelegramProvider, useTelegram } from "@/components/telegram-provider";
+import { User } from "@/db/schema/user";
 
 function ProfilePage() {
-  const { user, webApp } = useTelegram();
-  console.log(user);
+  const { user: tgUser } = useTelegram();
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    if (tgUser) {
+      fetch(`${process.env.URL}/api/user?id=${tgUser.id}`)
+        .then((res) => res.json())
+        .then((data) => setUser(data));
+    }
+  }, []);
 
   return (
     <div>
       {user ? (
         <div>
-          <h1>Welcome {user?.username}</h1>
+          <h1>Welcome {user?.name}</h1>
           User data:
           <pre>{JSON.stringify(user, null, 2)}</pre>
-          Eniter Web App data:
-          <pre>{JSON.stringify(webApp, null, 2)}</pre>
         </div>
       ) : (
         <div>Make sure web app is opened from telegram client</div>
