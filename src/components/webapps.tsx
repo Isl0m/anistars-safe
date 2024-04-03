81477599206315139be17d2a2a3b0a1942c4642c
Isl0m <islomrustam09@gmail.com>
2024-04-03 14:23:20 +0500

    telegram webapps

--- src/components/webapps.tsx
+++ src/components/webapps.tsx
@@ -1,9 +1,27 @@
 "use client";
 
-import { useInitData } from "@vkruglikov/react-telegram-web-app";
+import {
+  useInitData,
+  useShowPopup,
+  useThemeParams,
+} from "@vkruglikov/react-telegram-web-app";
 
 export function WebApps() {
   const [initDataUnsafe, initData] = useInitData();
+  const [colorScheme, themeParams] = useThemeParams();
+  const showPopup = useShowPopup();
 
+  const color = getComputedStyle(document.documentElement).getPropertyValue(
+    "--tg-theme-bg-color"
+  );
+  if (color) {
+    showPopup({ message: color });
+  }
+  if (themeParams.bg_color) {
+    document.documentElement.style.setProperty(
+      "--tg-theme-bg-color",
+      themeParams.bg_color
+    );
+  }
   return <h1>{initDataUnsafe?.user?.first_name}</h1>;
 }
