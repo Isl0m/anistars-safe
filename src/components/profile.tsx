069f96c448d200eeee40703bfa9d86b492d1ffe4
Isl0m <islomrustam09@gmail.com>
2024-04-04 07:05:42 +0500

    telegram webapps

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -29,6 +29,16 @@ function Profile({ filterOptions }: Props) {
     }
   }, [tgUser]);
 
+  if (!user) {
+    return (
+      <main className="flex min-h-screen flex-col gap-8 px-4 py-12 md:container">
+        <h1 className="text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Игрок не найден
+        </h1>
+      </main>
+    );
+  }
+
   return (
     <main className="flex min-h-screen flex-col gap-8 px-4 py-12 md:container">
       <h1 className="text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
