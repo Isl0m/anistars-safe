df4526a63f9e0ab52e03b2c9f889695d0f5b2f0e
Isl0m <islomrustam09@gmail.com>
2024-05-09 13:36:05 +0500

    joseph-redstar

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -38,7 +38,7 @@ export function Profile({ filterOptions }: Props) {
       </main>
     );
   }
-  // slime
+  // redstar
   return (
     <main className="flex min-h-screen flex-col gap-8 px-4 py-12 md:container">
       <h1 className="text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
