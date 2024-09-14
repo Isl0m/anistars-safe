264814f6969edcc3a2b409dd42a37a95f13a23a6
Isl0m <islomrustam09@gmail.com>
2024-09-14 16:57:12 +0500

    remove adaptive colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <Analytics />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen bg-background font-sans text-white antialiased",
           fontSans.variable
         )}
       >
